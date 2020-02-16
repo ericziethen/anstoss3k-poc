763c5a7e023b1602b877423e993cef796202de35
eric <eric@home.com>
2020-02-16 14:11:47 +1100

    Additional test

--- tests/engine/states/test_team_selection.py
+++ tests/engine/states/test_team_selection.py
@@ -1,2 +1,7 @@
-import
 
+from anstoss3k.engine.definitions import GameAction, GameState, State
+from anstoss3k.engine.states.team_selection import TeamSelectionState
+
+def test_next_state_complete_action():
+    state = TeamSelectionState('data')
+    assert state.next_state(GameAction.FINISH_MOVE) == GameState.PROGRESS_WEEK
